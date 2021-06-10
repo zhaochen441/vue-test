7a01e562acf995d4e1d5ae3f7d16785955de4f54
unknown <467311307@qq.com>
2021-06-10 19:18:18 +0800

    详情

--- taskOutPro/src/App.vue
+++ taskOutPro/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <van-tabbar v-model="active" route>
+    <!-- <van-tabbar v-model="active" route>
     <van-tabbar-item icon="home-o" replace to="/">主页</van-tabbar-item>
     <van-tabbar-item icon="search" replace to="/search">搜索</van-tabbar-item>
     <van-tabbar-item icon="bill-o" replace to="/orderform">订单</van-tabbar-item>
     <van-tabbar-item icon="friends-o" replace to="/mycount">我的</van-tabbar-item>
     
-    </van-tabbar>
+    </van-tabbar> -->
     <router-view/>
   </div>
 </template>
--- taskOutPro/src/components/CompanyData.vue
+++ taskOutPro/src/components/CompanyData.vue
@@ -18,16 +18,48 @@
         <van-col span="8"></van-col>
         </van-row>
         <div id="title">
-            <span>{{datalist.goodsScore}}评分</span>
-            <span>月销{{datalist.sales}}</span>
-            <span>约{{ datalist.deliveryTime }}分钟</span>
-            <span>距离{{ datalist.distance }}米</span>
+            <van-card id="card">
+                <template #tags>
+                    <van-tag plain type="danger" id="biaoqian">品牌</van-tag>
+                    &nbsp;&nbsp;&nbsp;
+                    <span>{{datalist.name}}</span>
+                    <br>
+                    <span>{{datalist.goodsScore}}评分</span>
+                    <span>月销{{datalist.sales}}</span>
+                    <span>约{{ datalist.deliveryTime }}分钟</span>
+                    <span>距离{{ datalist.distance }}米</span>
+                    
+                    <div> 
+                        <van-cell is-link @click="showPopup" >
+                            <span v-for="item in tag" :key="item.contents" class="youhuijuan">{{item.tag}}{{item.contents}}&nbsp;</span>
+                        
+                        </van-cell>
+                        <van-popup v-model="show" position="bottom" :style="{ height: '30%' }" />
+                    </div>
+                </template>
+            </van-card>
+            
+            <van-action-sheet v-model="show" title="优惠卷">
+                <div class="content"></div>
+            </van-action-sheet>
         </div>
-        
-            <van-action-sheet v-model="show" title="标题">
-            <div class="content">内容</div>
-            </van-action-sheet> 
-       
+        <van-tabs v-model="active" >
+                <van-tab title="点餐">
+                    <van-sidebar v-model="activeKey" class="left">
+                    <van-sidebar-item :title="item.name" v-for="item in topic" :key="item.id" @click="fenlei(item.id)"/>
+                    </van-sidebar>
+                    
+                </van-tab>
+                <van-tab title="评价">评价</van-tab>
+                <van-tab title="商家">商家</van-tab>
+            </van-tabs>
+            <div class="right">
+                        
+                       
+                
+                        
+                        
+             </div>
     </div>
 </template>
 <script>
@@ -38,13 +70,26 @@ export default {
         return{
             topic:"",
             datalist:"",
-            show:false
+            show:false,
+            tag:"",
+            active:"",
+            activeKey:0,
+            alldata:"",
+            footid:""
         }
     },
 
     methods:{
+        showPopup() {
+      this.show = true;
+    },
         to(){
             this.$router.go(-1)
+        },
+        fenlei(val){
+            this.$http.get("biz/queryFoodinfoByShopIdAndFoodCategoryId?shopId="+this.id+"&foodcategoryId="+val).then(function(res){
+                console.log(res.data)
+            })
         }
     },
     beforeRouteEnter(to,from,next){
@@ -53,6 +98,20 @@ export default {
                 console.log(res.data)
                 vm.datalist=res.data
             })
+            //商家分类信息
+            vm.$http.get("/biz/queryFoodCategory?id="+vm.id).then(function(res){
+            console.log(res.data)
+            vm.topic=res.data
+            vm.footid=res.data[0].id
+            console.log("商家分类id="+vm.footid)
+            
+        })
+        //分类查询商品
+        vm.$http.get('biz/queryFoodinfoByShopIdAndFoodCategoryId?shopId='+vm.id+'&foodcategoryId='+vm.footid[0]).then(function(res){
+             console.log("保存的分类id="+vm.footid)
+           console.log(res.data)
+           vm.alldata=res.data
+       })
         })
     },
     beforeRouteUpdate(to,from,next){
@@ -61,13 +120,26 @@ export default {
                 console.log(res.data)
                 that.datalist=res.data
             })
+            //商家分类信息
+            that.$http.get("/biz/queryFoodCategory?id="+that.id).then(function(res){
+            console.log(res.data)
+            that.topic=res.data
+            that.footid=res.data[0].id
+        })
+        //分类查询商品
+        that.$http.get("biz/queryFoodinfoByShopIdAndFoodCategoryId?shopId="+that.id+"&foodcategoryId="+that.footid[0]).then(function(res){
+        
+          
+           that.alldata=res.data
+       })
             next()
     },
     created(){
         var that=this
-        this.$http.get("/biz/queryFoodCategory?id="+this.id).then(function(res){
+        //优惠券
+        this.$http.get("/biz/querySpecialOfferByShopId?shopId="+this.id).then(function(res){
             console.log(res.data)
-            that.topic=res.data
+            that.tag=res.data
         })
     }
 }
@@ -76,9 +148,23 @@ export default {
 #title{
     text-align: center;
     font-size: 10px;
-    margin-top: 20px;
 }
 .content {
     padding: 16px 16px 160px;
   }
+  #card{
+      background: white;
+  }
+ #biaoqian{
+     background-color: yellow;
+ }
+ .left{
+     float: left;
+ }
+ .right span{
+     font-size: 12px;
+ }
+ .zsize{
+     position: static;
+ }
 </style>
