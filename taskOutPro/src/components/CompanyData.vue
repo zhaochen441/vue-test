5328b117970c3b59baa1fe0de4c567d3912215b4
unknown <467311307@qq.com>
2021-06-11 17:21:49 +0800

    商品详情部分完成

--- taskOutPro/src/components/CompanyData.vue
+++ taskOutPro/src/components/CompanyData.vue
@@ -43,23 +43,38 @@
                 <div class="content"></div>
             </van-action-sheet>
         </div>
-        <van-tabs v-model="active" >
+        <van-tabs v-model="active" @click="tabclick">
                 <van-tab title="点餐">
                     <van-sidebar v-model="activeKey" class="left">
-                    <van-sidebar-item :title="item.name" v-for="item in topic" :key="item.id" @click="fenlei(item.id)"/>
+                    <van-sidebar-item :title="item.name" v-for="item in topic" :key="item.id" @click="fenlei(item.id)" id="diancan"/>
                     </van-sidebar>
-                    
                 </van-tab>
-                <van-tab title="评价">评价</van-tab>
+                <van-tab title="评价">
+                    <div>
+                        <b>4.5</b>
+                        <p>综合评分</p>
+                        <span>高于周边商家99%</span>
+                    </div>
+                </van-tab>
                 <van-tab title="商家">商家</van-tab>
             </van-tabs>
-            <div class="right">
-                        
-                       
-                
-                        
-                        
+            <!-- 商家食品渲染 -->
+            <div v-if="this.set==0">
+                <div class="right" v-for="item in shopfood" :key="item.id">
+                <a href="#">
+                     <van-card class="zsize"
+                    num="2"
+                    price="2.00"
+                    :desc="item.info"
+                    :title="item.name"
+                    :thumb="'http://47.95.13.193:80/takeOutSystem-1.0-SNAPSHOT/'+item.photo"
+                    >
+                    </van-card>
+                </a>      
              </div>
+            </div>
+            <!-- 评价列表渲染 -->
+             
     </div>
 </template>
 <script>
@@ -68,18 +83,30 @@ export default {
     props:['id'],
     data(){
         return{
+            //商家介绍
             topic:"",
+            //商家距离好评等
             datalist:"",
             show:false,
+            //商家分类信息
             tag:"",
             active:"",
             activeKey:0,
             alldata:"",
-            footid:""
+            shopfood:"",
+            set:0
         }
     },
 
     methods:{
+        tabclick:function(name,val){
+            this.set=name
+            if(name==1){
+                this.$http.get("/biz//queryCommentByShopId?shopId="+this.id).then(function(res){
+                    console.log(res.data)
+                })
+            }
+        },
         showPopup() {
       this.show = true;
     },
@@ -87,10 +114,12 @@ export default {
             this.$router.go(-1)
         },
         fenlei(val){
+            var that=this
             this.$http.get("biz/queryFoodinfoByShopIdAndFoodCategoryId?shopId="+this.id+"&foodcategoryId="+val).then(function(res){
                 console.log(res.data)
+                that.shopfood=res.data
             })
-        }
+        }   
     },
     beforeRouteEnter(to,from,next){
         next(function(vm){
@@ -102,16 +131,16 @@ export default {
             vm.$http.get("/biz/queryFoodCategory?id="+vm.id).then(function(res){
             console.log(res.data)
             vm.topic=res.data
-            vm.footid=res.data[0].id
-            console.log("商家分类id="+vm.footid)
+            //分类查询商品
+            var id=res.data[0].id
+             vm.$http.get('biz/queryFoodinfoByShopIdAndFoodCategoryId?shopId='+vm.id+'&foodcategoryId='+id).then(function(res){
+                console.log(res.data)
+                //赋值给shopfood
+                vm.shopfood=res.data
+            })
             
         })
-        //分类查询商品
-        vm.$http.get('biz/queryFoodinfoByShopIdAndFoodCategoryId?shopId='+vm.id+'&foodcategoryId='+vm.footid[0]).then(function(res){
-             console.log("保存的分类id="+vm.footid)
-           console.log(res.data)
-           vm.alldata=res.data
-       })
+        
         })
     },
     beforeRouteUpdate(to,from,next){
@@ -124,14 +153,16 @@ export default {
             that.$http.get("/biz/queryFoodCategory?id="+that.id).then(function(res){
             console.log(res.data)
             that.topic=res.data
-            that.footid=res.data[0].id
+            //分类查询商品信息
+            var id=res.data[0].id
+             that.$http.get('biz/queryFoodinfoByShopIdAndFoodCategoryId?shopId='+that.id+'&foodcategoryId='+id).then(function(res){
+                console.log(res.data)
+                //赋值给shopfood
+                vm.shopfood=res.data
+            })
         })
         //分类查询商品
-        that.$http.get("biz/queryFoodinfoByShopIdAndFoodCategoryId?shopId="+that.id+"&foodcategoryId="+that.footid[0]).then(function(res){
-        
-          
-           that.alldata=res.data
-       })
+
             next()
     },
     created(){
@@ -164,7 +195,12 @@ export default {
  .right span{
      font-size: 12px;
  }
+
  .zsize{
      position: static;
+     
+ }
+ #diancan{
+     margin-top: 10px;
  }
 </style>
